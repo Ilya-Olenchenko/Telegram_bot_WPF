cb6934ba5fe5cbd4fa6891dc9a3fa7bd8a99816c
Ілля <olenchenko.ilya@ukr.net>
2022-02-03 21:16:52 +0200

    добавление кода в MainWindow

--- Telegram_bot_WPF/MainWindow.xaml.cs
+++ Telegram_bot_WPF/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Telegram.Bot;
+using System.Diagnostics;
 
 namespace Telegram_bot_WPF
 {
@@ -20,9 +13,51 @@ namespace Telegram_bot_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<TelegramUser> Users;
+        TelegramBotClient bot;
         public MainWindow()
         {
             InitializeComponent();
+            Users = new ObservableCollection<TelegramUser>();
+
+            userList.ItemsSource = Users;
+
+            string token = "5122212670:AAEVIgyIc5IlrlcWpmmchjaNzBVYLWEWEdw";
+
+            bot = new TelegramBotClient(token);
+
+            bot.OnMessage += delegate (object sebder, Telegram.Bot.Args.MessageEventArgs e)
+            {
+                string msg = $"{DateTime.Now}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+                File.AppendAllText("data.log", $"{msg}\n"); // Зберігання повідомлень в файл.
+
+                Debug.WriteLine(msg);
+                this.Dispatcher.Invoke(() => 
+                {
+                    var person = new TelegramUser(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                    if(!Users.Contains(person)) Users.Add(person);
+                    Users[Users.IndexOf(person)].AddMessage($"{person.Nick}: {e.Message.Text}");
+                });
+            };
+
+
+            bot.StartReceiving();
+            btnSendMsg.Click += delegate { SendMsg(); };
+            txtBxSendMsg.KeyDown += (s, e) => { if (e.Key == Key.Return) { SendMsg(); } };
+        }
+
+        public void SendMsg()
+        {
+            var concreteUser = Users[Users.IndexOf(userList.SelectedItem as TelegramUser)];
+            string responseMsg = $"Support: {txtBxSendMsg.Text}";
+            concreteUser.Messages.Add(responseMsg);
+
+            bot.SendTextMessageAsync(concreteUser.Id, txtBxSendMsg.Text);
+            string logText = $"{DateTime.Now}: >> {concreteUser.Id} {concreteUser.Nick} {responseMsg}\n";
+            System.IO.File.AppendAllText("data.log", logText);
+
+            txtBxSendMsg.Text = String.Empty;
         }
     }
 }
